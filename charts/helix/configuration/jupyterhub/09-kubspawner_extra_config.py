6c5027d3cb206f06f2c2bdcccd0939922c3ad93f
7066189 <nicolas.delahaye-ext@aphp.fr>
2026-05-04 16:04:40 +0200

    fix yaml / json for go and python

--- charts/helix/configuration/jupyterhub/09-kubspawner_extra_config.py
+++ charts/helix/configuration/jupyterhub/09-kubspawner_extra_config.py
@@ -1,14 +1,14 @@
-c.KubeSpawner.pod_security_context |  {
-                'runAsNonRoot': True,
-                'seccompProfile': {
-                    "type": {{- .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.type | default "RuntimeDefault" }},
-                    "localhostProfile": {{- .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.localhostProfile | default "" }},
-                    },					
-                },
+c.KubeSpawner.pod_security_context.update({
+    'runAsNonRoot': True,
+    'seccompProfile': {
+        'type': '{{- .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.type | default "RuntimeDefault" }}',
+        'localhostProfile': '{{- .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.localhostProfile | default "" }}',
+    },
+})
 
-c.KubeSpawner.container_security_context | {
-					"allowPrivilegeEscalation": False,
-					"capabilities": {
-						"drop": ["ALL"],
-					},
-				},
+c.KubeSpawner.container_security_context.update({
+    'allowPrivilegeEscalation': False,
+    'capabilities': {
+        'drop': ['ALL'],
+    },
+})
