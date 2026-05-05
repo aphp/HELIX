cb6436b01f443408143561b7cf8504c89083aa3f
7066189 <nicolas.delahaye-ext@aphp.fr>
2026-05-05 11:00:03 +0200

    Fix Kubeconform

--- charts/helix/configuration/jupyterhub/09-kubspawner_extra_config.py
+++ charts/helix/configuration/jupyterhub/09-kubspawner_extra_config.py
@@ -1,10 +1,19 @@
-c.KubeSpawner.pod_security_context.update({
+c.KubeSpawner.pod_security_context.update({{- if .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.type == "Localhost" }} 
+{
     'runAsNonRoot': True,
     'seccompProfile': {
-        'type': '{{- .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.type | default "RuntimeDefault" }}', 
-        {{- if .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.type == "Localhost" }} 'localhostProfile': '{{- .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.localhostProfile | default "" }}', {{- end }}
+        'type': 'localhost', 
+        'localhostProfile': '{{- .Values.configuration.jupyterhub.singleuser.securityContext.seccompProfile.localhostProfile | default "" }}',
     },
 })
+{{- else }}
+{
+    'runAsNonRoot': True,
+    'seccompProfile': {
+        'type': 'RuntimeDefault', 
+    },
+})
+{{- end }}
 
 c.KubeSpawner.container_security_context.update({
     'allowPrivilegeEscalation': False,
